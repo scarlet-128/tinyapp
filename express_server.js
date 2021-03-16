3a54ed90e45dd79e1bb035998604df9284d7dc4b
Scarlet Shao <scarletshao@gmail.com>
2021-03-16 23:09:48 +0000

    add the delete button

--- express_server.js
+++ express_server.js
@@ -3,6 +3,17 @@ const app = express();
 const PORT = 8080;
 
 const bodyParser = require("body-parser");
+const { request } = require("express");
+
+// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+function generateRandomString() {
+ return Math.random().toString(36).substring(2, 8);
+}
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,11 +23,7 @@ app.post("/urls", (req, res) => {
   console.log(req.body);  
   res.send("Ok");         
 });
-function generateRandomString() {
-  
-}
-app.use(bodyParser.urlencoded({extended: false}));
-app.set("view engine", "ejs");
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -42,6 +49,20 @@ app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase.shortURL;
   res.redirect(longURL);
 });
+
+
+
+
+
+
+app.post("/urls/:shortURL/delete",(req,res) => {
+  const shortURL = req.params.shortURL
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls")
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });

b588c35c53b11f2fb4e5291816348e7e26f87cd2
Scarlet Shao <scarletshao@gmail.com>
2021-03-16 05:10:36 +0000

    add the post method

--- express_server.js
+++ express_server.js
@@ -2,15 +2,27 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  res.send("Ok");         
+});
+function generateRandomString() {
+  
+}
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+app.get("/urls/new", (req, res) => {
+res.render("urls_new");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
